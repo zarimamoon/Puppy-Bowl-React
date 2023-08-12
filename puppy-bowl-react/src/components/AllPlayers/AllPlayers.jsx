0e2935460114b0c0748bd4cf93b788cd4fdb634e
Zarima Moon <zarima1991@yahoo.com>
2023-08-12 01:50:24 -0700

    added some styling changes

--- puppy-bowl-react/src/components/AllPlayers/AllPlayers.jsx
+++ puppy-bowl-react/src/components/AllPlayers/AllPlayers.jsx
@@ -31,7 +31,7 @@ const AllPlayers = () => {
     <>
       <section className="p-8 max-w-7xl mx-auto">
         <div className="text-center">
-          <h1 className="flex items-center justify-center text-center px-5 text-3xl font-bold lg:text-5xl text-white">
+          <h1 className="flex items-center justify-center text-center px-5 py-4 text-3xl font-bold lg:text-5xl text-white">
             Puppy Bowl
           </h1>
           
